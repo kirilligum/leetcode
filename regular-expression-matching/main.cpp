d352805f135319923504cbe25834ef55dfd8521e
Kirill Igumenshchev <bribeme@gmail.com>
2014-03-30 12:17:34 -0700

    reg ex done

--- regular-expression-matching/main.cpp
+++ regular-expression-matching/main.cpp
@@ -6,26 +6,23 @@
 
 using namespace std;
 
-bool ismatch(const char *s, const char *p) {
-  cout << " ismatch(" << *s<< "," << *p<<")\n";
-  if(*p=='\0') return *s=='\0' || s==nullptr ;
-  if(*p==*s) return ismatch(s+1,p+1);
-  if(*p=='.') return ismatch(s+1,p+1);
-  if(*p=='*') {
-    if(*(p+1)==*s) { 
-      cout << " for p+1 ismatch(" << *s<< "," << *(p+1)<<")\n";
-      if(ismatch(s,p+1)) {
-        return ismatch(nullptr,p+2);
-      }
-    } else return ismatch(s+1,p);
+bool isMatch(const char *s, const char *p) {
+  cout << " isMatch(" << *s<< "," << *p<<")\n";
+  if(*p=='\0') return *s=='\0';
+  if(*(p+1)!='*') return (*p==*s || (*p=='.' && *s!='\0')) && isMatch(s+1,p+1);
+  while(*p==*s || (*p=='.' && *s!='\0')) {
+    if(isMatch(s,p+2)) return 1;
+    ++s;
   }
-  return 0;
+  return isMatch(s,p+2);
 }
 
 int main(int argc, char const *argv[]) {
-  const char * s = {"cabaa"};
-  const char * p = {"*b"};
-  cout << (ismatch(s,p) ? "there is a pattern" : "there is no pattern") << endl;
+  const char * s = {"aab"};
+  cout << (isMatch(s,"c*a*b") ? "there is a pattern" : "there is no pattern") << endl;
+  cout << (isMatch(s,"c*d*b") ? "there is a pattern" : "there is no pattern") << endl;
+  cout << (isMatch(s,".*d*b") ? "there is a pattern" : "there is no pattern") << endl;
+  cout << (isMatch("ab",".*c") ? "there is a pattern" : "there is no pattern") << endl;
   cout << *s << endl;
   cout << s << endl;
   return 0;
